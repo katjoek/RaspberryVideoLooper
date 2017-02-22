f1c2db0a56bf88d7e140fd7cc9007c5bcee8bf16
Mark van de Veerdonk <markvandeveerdonk@gmail.com>
2017-02-22 09:38:10 +0000

    Improved script (still untested)

--- setup_raspberry.sh
+++ setup_raspberry.sh
@@ -6,26 +6,12 @@ set -e
 # have been executed:
 # * sudo apt-get update
 # * sudo apt-get install git omxplayer
-# * Via "sudo raspi-config" :
-#   * configure the board to login automatically without starting the GUI.
-#   * change locale and keyboard layout to en-us
 
 cd ~
 
-# Make sure the directory to mount usb to exists
-sudo mkdir /mnt/usb
-
-# Enable "airplane mode" by disabling Bluetooth and WiFi
-sudo echo "# WiFi" >  /etc/modprobe.d/raspi-blacklist.conf
-sudo echo "blacklist brcmfmac" >> /etc/modprobe.d/raspi-blacklist.conf
-sudo echo "blacklist brcmutil" >> /etc/modprobe.d/raspi-blacklist.conf
-sudo echo "# Bluetooth" >> /etc/modprobe.d/raspi-blacklist.conf
-sudo echo "blacklist btbcm" >> /etc/modprobe.d/raspi-blacklist.conf
-sudo echo "blacklist hci_uart" >> /etc/modprobe.d/raspi-blacklist.conf
-
-# Give GPU more memory in order to prevent green screen
-# https://www.raspberrypi.org/forums/viewtopic.php?f=28&t=73769
-sudo echo "gpu_mem=128" > /boot/config.txt
+# Auto login
+systemctl set-default multi-user.target
+ln -fs /etc/systemd/system/autologin@.service /etc/systemd/system/getty.target.wants/getty@tty1.service
 
 # "Fast boot". Don't wait for DHCP client.
 # Note problems reported here: https://github.com/RPi-Distro/repo/issues/24
@@ -39,7 +25,19 @@ sudo dpkg-reconfigure locales
 dpkg-reconfigure keyboard-configuration && printf "Reloading keymap. This may take a short while\n" && invoke-rc.d keyboard-setup start
 udevadm trigger --subsystem-match=input --action=change
 
+# Make sure the directory to mount usb to exists
+sudo mkdir /mnt/usb
+
+# Enable "airplane mode" by disabling Bluetooth and WiFi
+sudo echo "# WiFi" >  /etc/modprobe.d/raspi-blacklist.conf
+sudo echo "blacklist brcmfmac" >> /etc/modprobe.d/raspi-blacklist.conf
+sudo echo "blacklist brcmutil" >> /etc/modprobe.d/raspi-blacklist.conf
+sudo echo "# Bluetooth" >> /etc/modprobe.d/raspi-blacklist.conf
+sudo echo "blacklist btbcm" >> /etc/modprobe.d/raspi-blacklist.conf
+sudo echo "blacklist hci_uart" >> /etc/modprobe.d/raspi-blacklist.conf
+
 # Make sure video looping starts on startup
 echo "RaspberryVideoLooper/loop_videos.sh" >> /home/pi/.profile
 
 passwd
+
